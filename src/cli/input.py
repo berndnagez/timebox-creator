73ec96800267d2d39aea591a21e75b0ec5e58cf1
Björn Nagel <kontakt@bjoernnagel.de>
2026-02-08 22:21:10 +0100

    Improve submenu available types

--- src/cli/input.py
+++ src/cli/input.py
@@ -17,12 +17,12 @@ def read_valid_number(prompt, max_value):
 
 
 def show_main_menu():
-    print("\nWelcome to the Timebox Creator!")
-    print("Please select an option:")
-    print("(1) Create a new timebox")
-    print("(2) Create a new template")
-    print("(3) Load a template")
-    print("(4) Exit")
+    print(f"\nWelcome to the Timebox Creator!")
+    print(f"Please select an option:")
+    print(f"(1) Create a new timebox")
+    print(f"(2) Create a new template")
+    print(f"(3) Load a template")
+    print(f"(4) Exit")
     choice = read_valid_number("Enter your choice (1-4): ", 4)
     return choice
 
@@ -38,11 +38,11 @@ def read_nonempty_string(prompt, max_length=20):
             f"Empty input or input too long. Please enter a non-empty string with at most {max_length} characters.")
 
 
-def show_available_types():
-    available_timebox_types = timebox_types.get_types()
-    print("\nAvailable timebox types:")
+def show_available_types(available_timebox_types):
+    print(f"\nAvailable timebox types:")
     for option_id, timebox_type in enumerate(available_timebox_types, start=1):
         print(f"({option_id}) {timebox_type}")
+    return len(available_timebox_types)
 
 
 def read_valid_time(prompt):
@@ -53,9 +53,9 @@ def read_valid_time(prompt):
             if 0 <= hours < 24 and 0 <= minutes < 60:
                 return f"{hours:02d}:{minutes:02d}"
             else:
-                print("Invalid time. Please enter a valid time in HH:MM format.")
+                print(f"Invalid time. Please enter a valid time in HH:MM format.")
         except ValueError:
-            print("Invalid format. Please enter time in HH:MM format.")
+            print(f"Invalid format. Please enter time in HH:MM format.")
 
 
 def get_timezone():
@@ -67,13 +67,14 @@ def get_timezone():
 def show_create_timebox_menu():
     # NEXT Evaluierung der Eingaben
     title = read_nonempty_string(
-        "Enter the title of the timebox: ", max_length=40)
-    show_available_types()
-    type = read_valid_number("Enter your choice (1-5): ", 5)
-    start_time = read_valid_time("Enter the start time (HH:MM): ")
-    end_time = read_valid_time("Enter the end time (HH:MM): ")
+        f"Enter the title of the timebox: ", max_length=40)
+    number_of_available_types = show_available_types(timebox_types.get_types())
+    type = read_valid_number(
+        f"Enter your choice (1-{number_of_available_types}): ", number_of_available_types)
+    start_time = read_valid_time(f"Enter the start time (HH:MM): ")
+    end_time = read_valid_time(f"Enter the end time (HH:MM): ")
     description = read_nonempty_string(
-        "Enter notes for the timebox: ", max_length=200)
+        f"Enter notes for the timebox: ", max_length=200)
     zone = get_timezone()
     box = timebox_creator.create_box(
         title, start_time, end_time, description, type, zone)
