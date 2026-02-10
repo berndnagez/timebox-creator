9c11f23eb8610e2ee6b5e6116db5bf6f23582f07
Björn Nagel <kontakt@bjoernnagel.de>
2026-02-10 16:27:01 +0100

    Improve KeyboardInterrupt-handling

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from src.template import template
 from src.timebox import timebox_creator
 from src.output import ics_writer
@@ -7,29 +8,30 @@ timezone_conf_path = "./conf/timezone.json"
 
 
 def main():
-    try:
-        zone = timezone.get_timezone(timezone_conf_path)
-        choice = input.show_main_menu()
-        if choice == 1:
-            print("Creating a new timebox...")
-            box = input.show_create_timebox_menu(zone)
-            # Next fragen: Speichern oder als ics exportieren
-        elif choice == 2:
-            print("Creating a new template...")
-            # Implement template creation logic here
-        elif choice == 3:
-            print("Loading a template...")
-            loaded_template = template.read_template(
-                'templates/test_template.json')
-            cal = timebox_creator.create_events_from_template(loaded_template)
-            ics_writer.save_ical(cal, 'results/output.ics')
-        elif choice == 4:
-            print("Exiting the program. Goodbye!")
-            exit()
-    except KeyboardInterrupt:
-        print("\nKeyboard Interruption: Exiting the program. Goodbye!")
-        exit()
+    zone = timezone.get_timezone(timezone_conf_path)
+    choice = input.show_main_menu()
+    if choice == 1:
+        print("Creating a new timebox...")
+        box = input.show_create_timebox_menu(zone)
+        # Next fragen: Speichern oder als ics exportieren
+    elif choice == 2:
+        print("Creating a new template...")
+        # Implement template creation logic here
+    elif choice == 3:
+        print("Loading a template...")
+        loaded_template = template.read_template(
+            'templates/test_template.json')
+        cal = timebox_creator.create_events_from_template(loaded_template)
+        ics_writer.save_ical(cal, 'results/output.ics')
+    elif choice == 4:
+        print("Exiting the program. Goodbye!")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+
+    except KeyboardInterrupt:
+        print("\nKeyboard Interruption: Exiting the program. Goodbye!")
+        sys.exit(0)
