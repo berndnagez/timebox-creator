ec8137834fca3f25f73297455bef0abc752bd99f
Björn Nagel <kontakt@bjoernnagel.de>
2026-02-18 08:15:40 +0100

    Add timebox type

--- tests/test_timebox_types.py
+++ tests/test_timebox_types.py
@@ -2,7 +2,7 @@ from src.timebox import timebox_types
 
 
 def test_get_types():
-    expected_types = ["default", "15min-break",
+    expected_types = ["default", "5min-break", "15min-break",
                       "30min-break", "60min-break", "deep-work", "10min-rule", "pomodoro-classic"]
     returned_types = timebox_types.get_types()
     assert returned_types == expected_types
